e2eac653a2ba8456d91e1f2ef84a513274e1d863
Harrison Price <hprice518@gmail.com>
2020-04-14 22:23:58 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,12 +1,5 @@
 def introduction
 end
-def introduction(name)
+def introduction (name)
 end
-def instruction(name)
-  puts "Hi, my name is #{name}."
-end
-def introduction(name)
-  puts"Hi, my name is #{name}."
-end
-def introduction(Josh)
-end
+
